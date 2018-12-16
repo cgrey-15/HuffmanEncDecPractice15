e8bc07b0ad9d974ce79194cf05dc70bd117ae076
Christian Greyeyes <christian.greyeyes@gmail.com>
2018-12-15 19:16:39 -0700

    Revert "new version abound..."
    This reverts commit 5e2374befd6260e0d54f815347b657be846b5363.

--- helpme.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include<string>
-int main() {
-   std::string オウトプット君 = "まぁ眠いんだけど。。";
-   std::cout << オウトプット君 << '\n';
-}
--- huff_def.h
+++
@@ -1,9 +0,0 @@
-#ifndef __HUFF_DEF_H__
-#define __HUFF_DEF_H__
-
-#define ONES_32 0xffffffff
-#define WORD_LEN 32
-#define EOF_ABLE_ALPH_BITSPAN 9
-#define ONES_MASK ONES_32
-
-#endif
